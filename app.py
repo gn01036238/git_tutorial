d9f031a6cb940f7f012d05e21125fb07d8473412
Your Name <you@example.com>
2019-09-04 11:24:05 +0800

    make it better

--- app.py
+++ app.py
@@ -4,14 +4,13 @@
 # In[1]:
 
 
-from flask import Flask, escape, request
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    name = request.args.get("name", "World")
-    return f'Hello, {escape(name)}!'
+    return 'Hello, SPEEDY!'
 
 
 # In[2]:
@@ -34,7 +33,7 @@ if __name__ == "__main__":
     app.run(host='0.0.0.0',port=os.environ['PORT'])
 
 
-
+# In[ ]:
 
 
 
